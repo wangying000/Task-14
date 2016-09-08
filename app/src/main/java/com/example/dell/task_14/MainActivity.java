1328c96d376760bc81a434aaf68fb339750e7976
wangying000 <1178922618@qq.com>
2016-09-08 21:05:06 +0800

    Initial commit

---
+++ app/src/main/java/com/example/dell/task_14/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.dell.task_14;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent("com.example.dell.task_14.ACTION_START");
+                startActivity(intent);
+            }
+        });
+    }
+}
